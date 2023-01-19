dfae1577f538ca21e90a250d600871307158cff9
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-01-19 14:46:44 -0300

    Set movements

--- Assets/Scripts/MageController.cs
+++ Assets/Scripts/MageController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MageController : MonoBehaviour
 {
+    public  Animator    mageAnimator; //Adicionando o animator do gameobject sprite (filho)
+
     private Rigidbody2D mageRigidbody2D;
     public  float       moveSpeed;
     private float       touchRun = 0.0f;
@@ -13,9 +15,12 @@ public class MageController : MonoBehaviour
 
     public  GameObject  sprite; // Pegando o gameobject filho sprite, gameobject dos sprites e animações
 
+    
+
     // Start is called before the first frame update
     void Start()
     {
+        
         mageRigidbody2D = GetComponent<Rigidbody2D>(); //Adicionando Rigidbody a variável
         sprite = transform.GetChild(0).gameObject;
     }
@@ -23,10 +28,11 @@ public class MageController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        touchRun = Input.GetAxisRaw("Horizontal");
-        Debug.Log(touchRun);
-  
-        MageMove();
+        touchRun = Input.GetAxisRaw("Horizontal");  
+        MoveMage();
+
+        SetAnimations();
+
 
         if(touchRun < 0 && facingRight || touchRun > 0 && !facingRight)
         {
@@ -35,15 +41,24 @@ public class MageController : MonoBehaviour
         
     }
 
-    private void MageMove()
+    void MoveMage()
     {
         mageRigidbody2D.velocity = new Vector2(moveSpeed * touchRun, mageRigidbody2D.velocity.y);
     }
 
-    private void Flip()
+    void Flip()
     {
         facingRight = !facingRight;
         
         sprite.transform.localScale = new Vector3(-sprite.transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
+
+    
+
+    void SetAnimations()
+    {
+        
+        mageAnimator.SetBool("Running", mageRigidbody2D.velocity.x != 0);
+    }
+
 }
