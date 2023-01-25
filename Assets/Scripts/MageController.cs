e756279da34f42d5511d86495c4c7b28a7b3272f
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-01-25 14:41:38 -0300

    Test jump event

--- Assets/Scripts/MageController.cs
+++ Assets/Scripts/MageController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class MageController : MonoBehaviour
 {
-    public  Animator    mageAnimator; //Adicionando o animator do gameobject sprite (filho)
+    private  Animator    mageAnimator; //Adicionando o animator do gameobject sprite (filho)
 
     private Rigidbody2D mageRigidbody2D;
     public  float       moveSpeed;
@@ -18,12 +18,12 @@ public class MageController : MonoBehaviour
     public  GameObject  sprite; // Pegando o gameobject filho sprite, gameobject dos sprites e animações
 
     public  float       jumpForce;
-    private int         numberJumps;
-    public  int         maxJumps;
+    //private int         numberJumps;
+    //public  int         maxJumps;
 
     private bool isJumping = false;
-    private bool isFalling = false;
-    private bool isLanding = false;
+    //private bool isFalling = false;
+    //private bool isLanding = false;
 
 
     
@@ -31,7 +31,7 @@ public class MageController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        mageAnimator = GetComponent<Animator>();
         mageRigidbody2D = GetComponent<Rigidbody2D>(); //Adicionando Rigidbody a variável
         sprite = transform.GetChild(0).gameObject;
     }
@@ -42,29 +42,34 @@ public class MageController : MonoBehaviour
         isGround = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")); //Se tocar em algum tile colider, o isGround vira true
         mageAnimator.SetBool("IsGrounded", isGround); //Passando o true do isGround para o IsGrounded do animator
 
-        touchRun = Input.GetAxisRaw("Horizontal");  
+        touchRun = Input.GetAxisRaw("Horizontal");
         MoveMage();
 
         SetAnimations();
 
      
-        if (touchRun < 0 && facingRight || touchRun > 0 && !facingRight)
-        {
-            Flip();
-        }
+        
 
         if (Input.GetButtonDown("Jump"))
         {
-            JumpMage();
             isJumping = true;
+            JumpMage();
         }
         
     }
 
+    
+
     void MoveMage()
     {
-        mageRigidbody2D.velocity = new Vector2(moveSpeed * touchRun, mageRigidbody2D.velocity.y);
+        
+        mageRigidbody2D.velocity = new Vector2(moveSpeed * touchRun, 0f);
         //mageAnimator.SetBool("Running", mageRigidbody2D.velocity.x != 0);
+
+        if (touchRun < 0 && facingRight || touchRun > 0 && !facingRight)
+        {
+            Flip();
+        }
     }
 
     void Flip()
@@ -74,6 +79,17 @@ public class MageController : MonoBehaviour
         sprite.transform.localScale = new Vector3(-sprite.transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
+    void JumpMage()
+    {
+        if (isGround)
+        {
+            Debug.Log(isGround);
+            mageAnimator.SetBool("IsJumping", true);            
+        }
+        Debug.Log(isGround + "olá");
+               
+    }
+
     /*void JumpMage()
     {
         if (isGround)
@@ -81,41 +97,30 @@ public class MageController : MonoBehaviour
             numberJumps = 0;
         }
 
-        
-        if (numberJumps < maxJumps)
-        {
-            mageRigidbody2D.AddForce(new Vector2(0f, jumpForce));
-            isGround = false;
-            numberJumps++;
-        }
-        isJumping = false;        
-    }*/
-
-    void JumpMage()
-    {
-        if (isGround)
-        {
-            numberJumps = 0;
-        }
-
         if (numberJumps < maxJumps)
         {
             isGround = false;
             numberJumps++;
+            mageAnimator.SetBool("IsJumping", !isGround);
         }
         isJumping = false;
-    }
+    }*/
 
     public void AddForceJumpMage()
     {
+        
         mageRigidbody2D.AddForce(new Vector2(0f, jumpForce));
+        
+        isJumping = false;
+        Debug.Log(isJumping);
+        mageAnimator.SetBool("IsJumping", false);
     }
 
     void SetAnimations()
     {
         mageAnimator.SetFloat("EixoY", mageRigidbody2D.velocity.y);
         mageAnimator.SetBool("Running", mageRigidbody2D.velocity.x != 0 && isGround);
-        mageAnimator.SetBool("IsJumping", !isGround);
+        //mageAnimator.SetBool("IsJumping", !isGround);
     }
 
 }
