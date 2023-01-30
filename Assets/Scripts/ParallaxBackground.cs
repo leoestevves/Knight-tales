86f96e20fcf8141a9f41f3ca7b76cd66f17c67a8
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-01-30 16:58:59 -0300

    Second background parallax

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+
+    public Vector2 parallaxEffectMultiplier;
+
+    //private Sprite sprite;
+    //private Texture2D texture;
+    private float textureUnitSizeX;
+    private float textureUnitSizeY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeX = (texture.width / sprite.pixelsPerUnit) * transform.localScale.x;
+        textureUnitSizeY = (texture.height / sprite.pixelsPerUnit) * transform.localScale.y;
+                
+    }
+
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+        if(Mathf.Abs(cameraTransform.position.x - transform.position.x) >= textureUnitSizeX)
+        {
+            float offSetPositionX = (cameraTransform.position.x - transform.position.x) % textureUnitSizeX;
+            transform.position = new Vector3(cameraTransform.position.x + offSetPositionX, transform.position.y);
+        }
+    }
+}
