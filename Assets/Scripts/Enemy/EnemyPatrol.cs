7098ffe170ae2672fe2a5c588712d95c29c1782e
Leonardo Esteves <61708681+leoestevves@users.noreply.github.com>
2023-02-16 13:19:27 -0300

    Melee enemy script damage
    Wolf melee enemy script damage player

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform pontoA;
+    [SerializeField] private Transform pontoB;
+}
